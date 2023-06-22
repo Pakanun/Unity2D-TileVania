4058e40ee9399aba942f28758ecce761b280ff3c
Pakanun <dear_so17@hotmail.com>
2023-06-23 00:23:10 +0700

    Add movement and jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,23 +6,62 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float playerSpeed;
+    [SerializeField] float jumpSpeed;
+    
     Vector2 moveInput;
-
+    Rigidbody2D rb;
+    Animator anim;
+    CapsuleCollider2D myCollider;
+    
     
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        myCollider = anim.GetComponent<CapsuleCollider2D>();
     }
 
     
     void Update()
     {
-        
+        Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    void OnJump(InputValue value) 
+    { 
+        if(!myCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+        if(value.isPressed) 
+        {
+            rb.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * playerSpeed, rb.velocity.y);
+        rb.velocity = playerVelocity;
+
+        bool playerMove = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+
+        anim.SetBool("isRunning", playerMove);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+
+        if(playerHasHorizontalSpeed) 
+        { 
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
+
     }
 }
